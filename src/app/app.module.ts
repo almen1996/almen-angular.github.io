c346abaf90f27f756fa228b50599bdc0a09cc4a4
Almen Wong <almen.wong@bonjourhk.com>
2017-08-03 14:36:50 +0800

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FlexLayoutModule} from "@angular/flex-layout";
 import { AppComponent } from './app.component';
+import {AngularMaterialModule} from './ngmaterial.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlexLayoutModule,
+    AngularMaterialModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
